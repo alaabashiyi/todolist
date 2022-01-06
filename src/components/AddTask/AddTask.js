5aabbd0932c29c24aaacf4f6fd3fcd1c6398798b
Alaa Bashiyi <alaa.bashiy@gmail.com>
2022-01-05 20:17:54 -0800

    style fix

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -4,7 +4,7 @@ import './AddTask.scss';
 const AddTask = ({ text, onChange, onButtonClick, title }) => {
     return (
         <div className="addtask">
-            <input type="text" value={text} onChange={onChange} />
+            <input type="text" value={text} onChange={onChange} placeholder="Add todo..." />
             <Button title={title} onClick={onButtonClick} />
         </div>
     )
